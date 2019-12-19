8b951becabb26a47e6b353e3fc00bb4a9b0855bc
Jian Feng Ye <yejianfengblue@163.com>
2019-12-19 22:16:25 +0800

    Test to ExecutorService.invokeAny()

--- src/test/java/com/yejianfengblue/java/concurrency/CallableTest.java
+++ src/test/java/com/yejianfengblue/java/concurrency/CallableTest.java
@@ -1,5 +1,8 @@
 package com.yejianfengblue.java.concurrency;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -85,5 +88,76 @@ class CallableTest {
             log.info("{} - {}", i, resultNumber);
         }
         executor.shutdown();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    private static class UserValidator {
+
+        @Getter
+        private final String name;
+
+        private Logger log = LoggerFactory.getLogger(getClass());
+
+        UserValidator(String name) {
+            this.name = name;
+        }
+
+        /**
+         * Simulate the user validation process by sleep 1~9 second, and return a random boolean
+         * @return validation successful or not
+         */
+        boolean validate(String username, String password) {
+
+            try {
+                TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 10));
+            } catch (InterruptedException e) {
+                log.error("Error", e);
+                return false;
+            }
+
+            return ThreadLocalRandom.current().nextBoolean();
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static class UserValidationTask implements Callable<String> {
+
+        private final UserValidator userValidator;
+
+        private final String username;
+
+        private final String password;
+
+        private Logger log = LoggerFactory.getLogger(getClass());
+
+        @Override
+        public String call() throws Exception {
+
+            if (!userValidator.validate(username, password)) {
+                log.error("User cannot be validated by validator {}", userValidator.getName());
+                throw new Exception("User cannot be validated");
+            } else {
+                log.info("User is validated by validator {}", userValidator.getName());
+                return userValidator.getName();
+            }
+        }
+    }
+
+    @RepeatedTest(4)
+    void testExecutorServiceInvokeAny() throws ExecutionException, InterruptedException {
+
+        UserValidationTask ldapTask = new UserValidationTask(new UserValidator("LDAP"),
+                "testUsername", "testPassword");
+        UserValidationTask dbTask = new UserValidationTask(new UserValidator("DB"),
+                "testUsername", "testPassword");
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        log.info("START");
+        String result = executor.invokeAny(List.of(ldapTask, dbTask));
+
+        executor.shutdown();
+        executor.awaitTermination(60, TimeUnit.SECONDS);
+        log.info("END");
     }
 }
